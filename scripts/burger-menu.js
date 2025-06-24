eb27ee32d8f5114a002037fe5e775a9589f0fa8a
Yaroslav Avtonomov <avtnmv@icloud.com>
2025-06-24 23:36:02 +0300

    fix: burger-menu fixed

--- scripts/burger-menu.js
+++ scripts/burger-menu.js
@@ -2,15 +2,31 @@ const burgerMenu = document.getElementById("burger-menu");
 const burgerIcon = document.getElementById("burger-icon");
 const burgerLinks = document.querySelectorAll('.burger-menu a');
 
-const toggleMenu = (forceState = null) => {
-  const show = forceState !== null ? forceState : !burgerMenu.classList.contains('show');
-  burgerMenu.classList.toggle('show', show);
-  burgerIcon.classList.toggle('active', show);
-  document.body.classList.toggle('no-scroll', show);
-};
-
-burgerIcon.addEventListener('click', () => toggleMenu());
+burgerIcon.addEventListener('click', () => {
+  if (!burgerMenu.classList.contains('show')) {
+    burgerMenu.style.display = 'block';
+    setTimeout(() => {
+      burgerMenu.classList.add('show');
+      burgerIcon.classList.add('active');
+      document.body.classList.add('no-scroll');
+    }, 10);
+  } else {
+    burgerMenu.classList.remove('show');
+    burgerIcon.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+    setTimeout(() => {
+      burgerMenu.style.display = 'none';
+    }, 300); 
+  }
+});
 
 burgerLinks.forEach(link => {
-  link.addEventListener('click', () => toggleMenu(false));
+  link.addEventListener('click', () => {
+    burgerMenu.classList.remove('show');
+    burgerIcon.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+    setTimeout(() => {
+      burgerMenu.style.display = 'none';
+    }, 300);
+  });
 });
